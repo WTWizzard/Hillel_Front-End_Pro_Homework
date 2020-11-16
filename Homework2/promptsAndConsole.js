c0470338c929ea1453755c89de359243a80838c2
Suvorov Vitaliy <faringar35@gmail.com>
2020-11-16 23:52:03 +0200

    Homework-2: <Task> Random num to Console -done

--- Homework2/promptsAndConsole.js
+++ Homework2/promptsAndConsole.js
@@ -10,4 +10,13 @@ let forAvergeNum4 = +prompt('Please enter number: ');
 
 let Averge = (forAvergeNum1 + forAvergeNum2 + forAvergeNum3 + forAvergeNum4) / 4;
 
-console.log(`Averge number from your data is: ${Averge}`)
+console.log(`Averge number from your data is: ${Averge}`);
+//Random number to Console(5 different methods)
+
+let randomNumber = Math.floor(Math.random() * (10 - 1) + 1);
+
+console.log(randomNumber);
+console.info(randomNumber);
+console.error(randomNumber);
+console.warn(randomNumber);
+console.dir(randomNumber);
