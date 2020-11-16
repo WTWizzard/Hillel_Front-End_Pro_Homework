ccf025287fd4ce66822ef48585e2b12556eae157
Suvorov Vitaliy <faringar35@gmail.com>
2020-11-16 14:14:38 +0200

    Homework-2: <Task> Averge Number(easy variant) -done

--- Homework2/promptsAndConsole.js
+++ Homework2/promptsAndConsole.js
@@ -3,3 +3,11 @@
 let usersInput = +prompt('Please enter year when you born: ');
 console.log(`Your age is: ${2020 - usersInput} years old`);
 //Averge numb
+let forAvergeNum1 = +prompt('Please enter number: ');
+let forAvergeNum2 = +prompt('Please enter number: ');
+let forAvergeNum3 = +prompt('Please enter number: ');
+let forAvergeNum4 = +prompt('Please enter number: ');
+
+let Averge = (forAvergeNum1 + forAvergeNum2 + forAvergeNum3 + forAvergeNum4) / 4;
+
+console.log(`Averge number from your data is: ${Averge}`)
