5eab4b8ba43beb0b0992d157c747d1a2b8f211e4
Suvorov Vitaliy <faringar35@gmail.com>
2020-11-16 12:36:29 +0200

    Homework-2: <Task> Age of the User -changes

--- Homework2/promptsAndConsole.js
+++ Homework2/promptsAndConsole.js
@@ -1,4 +1,5 @@
 'use strict';
 //Age of the User
-const usersInput = +prompt('Please enter year when you born: ');
+let usersInput = +prompt('Please enter year when you born: ');
 console.log(`Your age is: ${2020 - usersInput} years old`);
+//Averge numb
