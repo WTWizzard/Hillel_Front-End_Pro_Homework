7d094fb58864cd41bf62ef3056427a0b1c1039c8
Suvorov Vitaliy <faringar35@gmail.com>
2020-11-22 21:06:32 +0200

    Homework-3: <Task> Work Interview -done

--- Homework3/conditions.js
+++ Homework3/conditions.js
@@ -11,3 +11,47 @@ if (isNaN(usersAge)) {
 } else {
   console.log("Как говорится, Welcome");
 }
+//Task: "Work interview"
+
+let employeeAge  = +prompt('Ваш возраст: ')
+let employeeExp = confirm('Ваш опыт больше 3-х лет')
+let employeeEng = prompt('Укажите ваш уровень английского (B1, B2, C1 или C2):').toLocaleLowerCase();
+
+let result = 0;
+
+switch (employeeEng) {
+    case 'c2':
+        result += 5; 
+        break;
+    case 'c1': 
+        result += 4;
+        break;
+    case 'b2':
+        result += 3;
+        break;
+    case 'b1':
+        result += 2;
+        break;
+    default:
+        result += 1;
+}
+
+result += employeeExp ? 5 : 2;
+
+if(employeeAge >= 30){
+    result += 5;
+}else if (employeeAge < 18){
+    result += 0;
+}else{
+    result += 2;
+}
+
+if(result === 15){
+    console.log('Вы прошли!')
+}else if(result < 15 && result >= 12){
+    console.log('Вы прошли, но на меньшую зарплату')
+}else if(result < 12 && result >= 8){
+    console.log('"Мы вам перезвоним"')
+}else{
+    console.log('До свидания')
+}
