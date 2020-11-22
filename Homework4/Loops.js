af4b1d917bb170e51c8b8f7fd5c279b41ef39b70
Suvorov Vitaliy <faringar35@gmail.com>
2020-11-22 23:57:28 +0200

    Homework-4: <Task> Iteration of even  numbers all variations -done

--- Homework4/Loops.js
+++ Homework4/Loops.js
@@ -32,3 +32,37 @@ for(let i = 1; i < numbForIteration; i++){
         console.log(i);
     }
 }
+
+
+//Task: Iteation of even numbers all variations
+
+//while
+
+const numbForIteration = 50;
+
+let index3 = 1;
+
+while(index3 < numbForIteration){
+    if(!(index3 % 2)){
+        console.log(index3);
+    }
+    index3++;
+}
+
+//do while
+
+let index4 = 1;
+do{
+    if(!(index4 % 2)){
+        console.log(index4);
+    }
+    index4++;
+}while(index4 < numbForIteration )
+
+//for
+
+for(let i = 1; i < numbForIteration; i++){
+    if(!(i % 2)){
+        console.log(i);
+    }
+}
