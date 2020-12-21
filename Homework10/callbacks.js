64fb6c62378773830ba9a993769542b20c3decc9
Suvorov Vitaliy <faringar35@gmail.com>
2020-12-21 16:00:08 +0200

    Homework-10: Method some -done

--- Homework10/callbacks.js
+++ Homework10/callbacks.js
@@ -30,4 +30,30 @@ Array.prototype.forEach_2 = function (cb, thisArg){
     }
 }
 
+Array.prototype.some_2 = function (cb, thisArg){
+    if(typeof cb !== 'function'){
+        throw new TypeError(`${cb} is not a function`);
+    }
 
+    if(this === null){
+        throw new TypeError(' this is null or not defined')
+    }
+
+    const obj = Object(this);
+
+    let l = obj.length;
+
+    let _this;
+
+    if(arguments.length > 1){
+        _this = thisArg;
+    }
+
+    for (let key = 0; key < obj.length; key++) {
+        let keyValue = obj[key];
+        if(key in obj && cb.call(_this, keyValue, key, obj)){
+            return true;
+        }
+    }
+    return false;
+}
