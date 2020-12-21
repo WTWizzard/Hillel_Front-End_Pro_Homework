52d9e46cb4a6b63c270e03977650c38288befb0f
Suvorov Vitaliy <faringar35@gmail.com>
2020-12-21 17:43:26 +0200

    Homework-10: changes

--- Homework10/callbacks.js
+++ Homework10/callbacks.js
@@ -1,57 +1,21 @@
 'use strict';
 
 
-Array.prototype.forEach_2 = function (cb, thisArg){
-    if(typeof cb !== 'function'){
-        throw new TypeError(`${cb} is not a function`);
-    }
+const forEach_2 = function (cb, arr){
 
-    if(this === null){
-        throw new TypeError(' this is null or not defined')
-    }
-
-    const obj = Object(this);
-
-    let l = obj.length;
-
-    let _this;
-
-    if(arguments.length > 1){
-        _this = thisArg;
-    }
-
-    for (let key = 0; key < l; key++) {
-        let keyValue;
-
-        if(key in obj){
-            keyValue = obj[key];
-            cb.call(_this, keyValue, key, obj);
+    for (let key = 0; key < arr.length; key++) {
+        let keyValue; 
+        if(key in arr){
+            keyValue = arr[key];
+            cb.apply(arr, [keyValue, key, arr]);
         }
     }
 }
+const some_2 = function (cb, arr){
 
-Array.prototype.some_2 = function (cb, thisArg){
-    if(typeof cb !== 'function'){
-        throw new TypeError(`${cb} is not a function`);
-    }
-
-    if(this === null){
-        throw new TypeError(' this is null or not defined')
-    }
-
-    const obj = Object(this);
-
-    let l = obj.length;
-
-    let _this;
-
-    if(arguments.length > 1){
-        _this = thisArg;
-    }
-
-    for (let key = 0; key < obj.length; key++) {
-        let keyValue = obj[key];
-        if(key in obj && cb.call(_this, keyValue, key, obj)){
+    for (let key = 0; key < arr.length; key++) {
+        let keyValue = arr[key];
+        if(key in arr && cb.apply(arr, [keyValue, key, arr])){
             return true;
         }
     }
