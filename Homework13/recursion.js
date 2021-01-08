b514f7041c5982cff5b175f9728f4233c7a955ce
Suvorov Vitaliy <faringar35@gmail.com>
2021-01-08 16:52:29 +0200

    Homework-13: <Task-5> -done

--- Homework13/recursion.js
+++ Homework13/recursion.js
@@ -82,3 +82,29 @@ const obj2 = task4(obj);
 obj2.b.c =12;
 
 console.log(obj, obj2)
+
+/*
+    Task V: Дан объект с N вложенностями. При передаче строки вида 'a.b.c' должно вернуться значение, 
+    находящееся внутри вложенности, указанной через точку. Либо, если значения не существует - undefined
+*/
+
+const task5 = (obj,path) =>{
+    let value, lane;
+    
+    if(Array.isArray(path)){
+    	path.shift();
+      lane = path;
+    }else{
+    	lane = path.split('.')
+    }
+
+    if (typeof obj !== "object" || obj === null) return obj;
+
+    for (const el of lane) {
+        value = obj[el]
+
+        return task5(value, lane)   
+    }
+}
+
+console.log(task5(obj2, 'b.c'))
