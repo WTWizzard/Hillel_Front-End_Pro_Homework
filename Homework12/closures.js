25fa71ba2480dbe1d8ac70f680a8ee377bb7ae7d
Suvorov Vitaliy <faringar35@gmail.com>
2021-01-15 22:25:23 +0200

    Homework-12: <Task-1> Now definitely -done

--- Homework12/closures.js
+++ Homework12/closures.js
@@ -2,17 +2,21 @@
 
 //task I
 
-function curry(f, a, b){
-    return function(c,d){
-        return f(a,b,c,d)
-    }
-}
+// function curry(f, a, b){
+//     return function(c,d){
+//         return f(a,b,c,d)
+//     }
+// }
 
-function sum(a,b,c,d) { return a+b+c+d; }
+const curry = (f, ...numbers) => (...numbers2) => f(...numbers, ...numbers2)
 
-const curriedFunction = curry(sum, 1,2); // запоминает для вызова sum 2 параметра
+// function sum(a,b,c,d) { return a+b+c+d }
 
-console.log(curriedFunction(3,4)) // выводит 10 т.е. выполняется функционал sum(1,2,3,4).
+const sum = (...nums) => nums.reduce((acc, n) => acc + n, 0)
+
+const curriedFunction = curry(sum, 1,2,142); // запоминает для вызова sum N параметров
+
+console.log(curriedFunction(1,2,2)) // выводит 150
 
 
 //task II
